9ff9369901fadc34590046e51700e06bd69b78e1
thehorizon-byte <63099475+thehorizon-byte@users.noreply.github.com>
2020-05-27 14:26:29 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ASSIGNMENT II
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set_x <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get_x <- function() x
+    set_i <- function(inverse) i <<- inverse
+    get_i <- function() i
+    list (set_x = set_x, get_x = get_x,
+          set_i = set_i, get_i = get_i)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$get_i()
+    if (!is.null(i)) {
+        message("Congratulations. It was cached. :)")
+        return(i)
+    }
+    i <- solve(x$get_x())
+    x$set_i(i)
+    i
 }
+
+a <- makeCacheMatrix(matrix(rexp(100),10,10))
+cacheSolve(a)
+cacheSolve(a)
+cacheSolve(a)
+
+a$set_x(matrix(rexp(100),10,10))
+cacheSolve(a)
+cacheSolve(a)
